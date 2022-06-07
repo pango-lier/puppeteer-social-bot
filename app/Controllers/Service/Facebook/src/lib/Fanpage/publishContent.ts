997c93d6c18e57228e22e3d8bbc5f7ec2b7de50d
Binh Trong <binhtrongcdt1@gmail.com>
2022-06-07 10:04:26 +0700

    post page with image

--- app/Controllers/Service/Facebook/src/lib/Fanpage/publishContent.ts
+++ app/Controllers/Service/Facebook/src/lib/Fanpage/publishContent.ts
@@ -33,13 +33,26 @@ const actionPublishContent = async (func: PuppeteerActionFunc) => {
 
 const selectImages = async (func, images): Promise<string[]> => {
   if (images) {
+    if (
+      await func.checkSelector(
+        "div:nth-child(2) > .tojvnm2t > .oajrlxb2 > div > div > .tv7at329 > .iyyx5f41 > .bp9cbjyn > .hu5pjgll"
+      )
+    ) {
+      return await func.uploadImage(
+        images,
+        "div:nth-child(2) > .tojvnm2t > .oajrlxb2 > div > div > .tv7at329 > .iyyx5f41 > .bp9cbjyn > .hu5pjgll"
+      );
+    }
+    await func.click(
+      ".dwxx2s2f:nth-child(1) > div:nth-child(1) > .tojvnm2t:nth-child(1) > .oajrlxb2:nth-child(1) > div:nth-child(1) > div:nth-child(1) > .tv7at329:nth-child(1) > .iyyx5f41:nth-child(1) > .bp9cbjyn:nth-child(1) > .hu5pjgll:nth-child(1)"
+    );
     // await func.click(
-    //   ".dwxx2s2f:nth-child(1) > div:nth-child(1) > .tojvnm2t:nth-child(1) > .oajrlxb2:nth-child(1) > div:nth-child(1) > div:nth-child(1) > .tv7at329:nth-child(1) > .iyyx5f41:nth-child(1) > .bp9cbjyn:nth-child(1) > .hu5pjgll:nth-child(1)"
+    //   ".l9j0dhe7 > .rq0escxv > .rq0escxv > .s45kfl79 > .hu5pjgll"
     // );
-    // await func.delay(2);
+    await func.delay(2);
     return await func.uploadImage(
       images,
-      "div:nth-child(2) > .tojvnm2t > .oajrlxb2 > div > div > .tv7at329 > .iyyx5f41 > .bp9cbjyn > .hu5pjgll"
+      ".l9j0dhe7 > .rq0escxv > .rq0escxv > .s45kfl79 > .hu5pjgll"
     );
   }
   return [];
--- app/Controllers/Service/Facebook/src/lib/Login.ts
+++ app/Controllers/Service/Facebook/src/lib/Login.ts
@@ -7,9 +7,9 @@ export const Login = async (profile: Profile) => {
   const pup: PuppeteerInterface = await BrowserProfile.StartUp();
   await login(pup.func, profile);
   const url =
-    "https://www.facebook.com/Website-115041168916224";
+    "https://www.facebook.com/T%C3%A2m-Nh%C3%B4m-K%C3%ADnh-%C4%90%C3%A0-L%E1%BA%A1t-108625631775421";
   let images = [
-    "https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/283954709_131284709509513_445255171833006986_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=p8kgYQ9PvEMAX_nHHh8&tn=Fd2Dw3XRuxeewy6A&_nc_ht=scontent.fdad1-3.fna&oh=00_AT8LuuyCcseLxstzqOpk9WFdTg4a3jjFRGOmOdmzMtt8Cg&oe=6296209D",
+    "https://nhomkinhdalat.com/storage/post/t164765729689.jpg",
   ];
   await Fanpage.publishContent(pup, {
     url: url,
