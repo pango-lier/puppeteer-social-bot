f021bc8c86c263dac10124f66ce7d6056cac322b
Binh Trong <binhtrongcdt1@gmail.com>
2022-06-21 23:48:58 +0700

    get url

--- app/Controllers/Service/Youtube/src/lib/Short/index.ts
+++ app/Controllers/Service/Youtube/src/lib/Short/index.ts
@@ -1,4 +1,5 @@
 import { PuppeteerInterface } from "App/Controllers/Service/Facebook/src/Interface";
+import { random } from "App/Controllers/Service/Facebook/src/utils";
 import Youtube from "../../..";
 import { dowload } from "../YoutubeDl";
 
@@ -6,17 +7,30 @@ class Short {
   async dowloadVideo(page: PuppeteerInterface) {
     await Youtube.Login.goto(page);
     await Youtube.Login.gotoShort(page);
-    await page.page.exposeFunction("onHashChange", (url) =>
-      page.page.emit("hashchange", url)
-    );
-    await page.page.evaluateOnNewDocument(() => {
-      addEventListener("hashchange", (e) => {
-        console.log(location.href);
-      });
-    });
+    let count = 0;
+    const currentUrl = () => {
+      return window.location.href;
+    };
+    const links: string[] = [];
 
-    // Listen for hashchange events in node Puppeteer code.
-    page.page.on("hashchange", (url) => console.log("hashchange event:", url));
+    const interval = setInterval(async () => {
+      const href: string = await page.page.evaluate(currentUrl);
+      console.log(`${count} current URL is: ${href}`);
+      count++;
+      // await page.page.mouse.wheel({ deltaY: 250 * count });
+      if (random(0, 10) === 1) {
+        await page.page.click(
+          "#navigation-button-down > .style-scope > .yt-simple-endpoint > #button > #button > .style-scope"
+        );
+      }
+      if (!links.includes(href)) {
+        links.push(href);
+      }
+    }, 400);
+
+    await page.func.delay(100);
+    console.log(links);
+    await clearInterval(interval);
     // const url = "https://www.youtube.com/shorts/gPz-eLT8RIw" + "'";
     // await dowload(url);
   }
