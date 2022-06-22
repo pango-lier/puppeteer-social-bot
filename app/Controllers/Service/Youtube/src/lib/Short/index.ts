65214b897dbcb2f32ecddbb8b0b18203940f866f
Binh Trong <binhtrongcdt1@gmail.com>
2022-06-22 13:45:42 +0700

    wip

--- app/Controllers/Service/Youtube/src/lib/Short/index.ts
+++ app/Controllers/Service/Youtube/src/lib/Short/index.ts
@@ -1,12 +1,19 @@
 import { PuppeteerInterface } from "App/Controllers/Service/Facebook/src/Interface";
 import { random } from "App/Controllers/Service/Facebook/src/utils";
 import Youtube from "../../..";
-import { dowload } from "../YoutubeDl";
+import YoutubeDl from "../YoutubeDl";
 
+const BUTTON_DOWN =
+  "#navigation-button-down > .style-scope > .yt-simple-endpoint > #button > #button > .style-scope";
 class Short {
-  async dowloadVideo(page: PuppeteerInterface) {
-    await Youtube.Login.goto(page);
-    await Youtube.Login.gotoShort(page);
+  async getLinks(
+    page: PuppeteerInterface,
+    {
+      time = 100,
+      intervalCLick = 400,
+      enableDowload = false,
+    }: { time: number; intervalCLick: number; enableDowload: boolean }
+  ) {
     let count = 0;
     const currentUrl = () => {
       return window.location.href;
@@ -15,24 +22,35 @@ class Short {
 
     const interval = setInterval(async () => {
       const href: string = await page.page.evaluate(currentUrl);
-      console.log(`${count} current URL is: ${href}`);
+      // console.log(`${count} current URL is: ${href}`);
       count++;
       // await page.page.mouse.wheel({ deltaY: 250 * count });
       if (random(0, 10) === 1) {
-        await page.page.click(
-          "#navigation-button-down > .style-scope > .yt-simple-endpoint > #button > #button > .style-scope"
-        );
+        await page.page.click(BUTTON_DOWN);
       }
       if (!links.includes(href)) {
         links.push(href);
+        if (enableDowload) YoutubeDl.dowload(href + "'");
       }
-    }, 400);
+    }, intervalCLick);
 
-    await page.func.delay(100);
-    console.log(links);
+    await page.func.delay(time);
     await clearInterval(interval);
+    return links;
+  }
+  async dowloadVideo(page: PuppeteerInterface) {
+    await Youtube.Login.goto(page);
+    await Youtube.Login.gotoShort(page);
+    const links = await this.getLinks(page, {
+      time: 100,
+      intervalCLick: 400,
+      enableDowload: true,
+    });
+    // for (const url of links) {
+    //   dowload(url + "'");
+    // }
     // const url = "https://www.youtube.com/shorts/gPz-eLT8RIw" + "'";
-    // await dowload(url);
+    //
   }
 }
 export default Short;
--- app/Controllers/Service/Youtube/src/lib/YoutubeDl/index.ts
+++ app/Controllers/Service/Youtube/src/lib/YoutubeDl/index.ts
@@ -4,32 +4,36 @@ import {
 } from "App/Controllers/Service/Facebook/src/utils";
 import youtubedl from "youtube-dl-exec";
 
-export const dowload = async (url: string) => {
-  let path: string | undefined = undefined;
-  const ouput = await youtubedl(url, {
-    dumpSingleJson: true,
-    noWarnings: true,
-    // noCallHome: true,
-    noCheckCertificate: true,
-    preferFreeFormats: true,
-    youtubeSkipDashManifest: true,
-    referer: url,
-  });
-  for (const format of ouput.formats) {
-    if (format.vcodec !== "none" && format.acodec !== "none") {
-      const d = new Date();
-      path = await downloadFile(
-        format.url,
-        "vi" + d.getTime() + "_" + random(1000, 1000000000) + "." + format.ext
-      );
-      break;
+class YoutubeDl {
+  dowload = async (url: string) => {
+    let path: string | undefined = undefined;
+    const output = await youtubedl(url, {
+      dumpSingleJson: true,
+      noWarnings: true,
+      // noCallHome: true,
+      noCheckCertificate: true,
+      preferFreeFormats: true,
+      youtubeSkipDashManifest: true,
+      referer: url,
+    });
+    for (const format of output.formats) {
+      if (format.vcodec !== "none" && format.acodec !== "none") {
+        const d = new Date();
+        path = await downloadFile(
+          format.url,
+          "vi" + d.getTime() + "_" + random(1000, 1000000000) + "." + format.ext
+        );
+        break;
+      }
     }
-  }
-  return {
-    url: url,
-    tags: ouput.tags,
-    description: ouput.description,
-    ext: ouput.ext,
-    path: path,
+    return {
+      url: url,
+      tags: output.tags,
+      description: output.description,
+      ext: output.ext,
+      path: path,
+    };
   };
-};
+}
+
+export default new YoutubeDl();
